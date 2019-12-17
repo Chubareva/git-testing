8670ffcfd535558e9b289a9a592cafcbec889b92
Inga Chubareva <ingach23@gmail.com>
2019-12-17 20:58:32 +0300

    testing

--- RentTests/src/test/java/by/bsu/util/StringUtil.java
+++
@@ -1,6 +0,0 @@
-package by.bsu.util;
-
-public class StringUtil {
-    public static final String ERROR_MESSAGE = "PLEASE CHECK YOUR INFORMATION  ";
-    public static final String ERROR_MESSAGE_WITH_INFO = "PLEASE CHECK YOUR DATA";
-}
---
+++ unitTest/src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(5, 5, 5);
+        boolean canBuild = TriangleValidation.canBuildTriangle(triangle);
+
+        System.out.println("Can I draw a triangle with sides: 5, 5, 5?");
+
+        if (canBuild) {
+            System.out.println("--> Yes, you can!");
+        } else {
+            System.out.println("--> No! =(");
+        }
+    }
+}
+
